e8ac0cd75345b5f62e29dce8116eb24a6018337a
longkey1 <longkey1@gmail.com>
2023-05-01 22:10:01 +0900

    fix: build and sub shell scripts

--- .dotfiles/jnal/build.sh
+++ .dotfiles/jnal/build.sh
@@ -2,11 +2,11 @@
 
 BINARY="jnal"
 REPOSITORY="longkey1/jnal"
+CURRENT=$(cd $(dirname $0);pwd)
 
 if [ ! -x "${LOCAL_BIN}/${BINARY}" ]; then
   ${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/
 fi
 
 #
-CURRENT=$(cd $(dirname $0);pwd)
-${LOCAL_BIN}/checkexec ${LOCAL_CONFIG}/jnal/config.toml ${LOCAL_CONFIG}/jnal/config.toml.dist -- env LOCAL_CONFIG=${LOCAL_CONFIG} ${CURRENT}/build_envsubst.sh
+${LOCAL_BIN}/checkexec ${LOCAL_CONFIG}/jnal/config.toml ${LOCAL_CONFIG}/jnal/config.toml.dist -- env LOCAL_CONFIG=${LOCAL_CONFIG} ${CURRENT}/build_config.sh
