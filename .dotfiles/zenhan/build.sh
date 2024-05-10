6310fb9abad9a2e17b5f0b5186bb6e241f8adb04
longkey1 <longkey1@gmail.com>
2024-05-10 10:44:03 +0900

    fix: lroom golang version

--- .dotfiles/zenhan/build.sh
+++ .dotfiles/zenhan/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
+if [[ "$(uname -r)" != *microsoft* ]]; then
+  exit 0
+fi
+
 DIRECTORY="zenhan"
 REPOSITORY="iuchim/zenhan"
 CURRENT=$(cd $(dirname $0);pwd)
 WIN_BIN="/mnt/c/Users/$(powershell.exe '$env:USERNAME' | tr -d '\r')/Program Files"
-
-if [[ "$(uname -r)" != *microsoft* ]]; then
-  exit 0
-fi
 
 if [ ! -d "${WIN_BIN}/${DIRECTORY}" ]; then
   ${LOCAL_BIN}/eget ${REPOSITORY} --file zenhan --to ${WIN_BIN}/
