d8c0e3d0bf8e74908c9f2c47e00d4dc7f833ad0c
longkey1 <longkey1@gmail.com>
2024-05-10 09:13:29 +0900

    fix: build zenhan

--- .dotfiles/zenhan/build.sh
+++ .dotfiles/zenhan/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env zsh
 
-BINARY="zenhan.exe"
+DIRECTORY="zenhan"
 REPOSITORY="iuchim/zenhan"
 CURRENT=$(cd $(dirname $0);pwd)
-WIN_BIN="/mnt/c/Users/$(powershell.exe '$env:USERNAME' | tr -d '\r')/Program Files/zenhan"
+WIN_BIN="/mnt/c/Users/$(powershell.exe '$env:USERNAME' | tr -d '\r')/Program Files"
 
 if [[ "$(uname -r)" != *microsoft* ]]; then
   exit 0
 fi
 
-if [ ! -x "${WIN_BIN}/${BINARY}" ]; then
-  ${LOCAL_BIN}/eget ${REPOSITORY} --to ${WIN_BIN}/${BINARY}
+if [ ! -d "${WIN_BIN}/${DIRECTORY}" ]; then
+  ${LOCAL_BIN}/eget ${REPOSITORY} --file zenhan --to ${WIN_BIN}/
 fi
