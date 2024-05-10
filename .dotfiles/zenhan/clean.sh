f4507cacb0845b1caefcafdba2381058922e52ff
longkey1 <longkey1@gmail.com>
2024-05-10 10:44:03 +0900

    fix: lroom golang version

--- .dotfiles/zenhan/clean.sh
+++ .dotfiles/zenhan/clean.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh
 
-BINARY="zenhan.exe"
-WIN_BIN="/mnt/c/Users/$(powershell.exe '$env:USERNAME' | tr -d '\r')/Program Files/zenhan"
+if [[ "$(uname -r)" != *microsoft* ]]; then
+  exit 0
+fi
+
+DIRECTORY="zenhan"
+WIN_BIN="/mnt/c/Users/$(powershell.exe '$env:USERNAME' | tr -d '\r')/Program Files/${DIRECTORY}"
 
 rm -rf ${WIN_BIN}
