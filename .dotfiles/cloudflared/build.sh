cab754d10d796e66c4be5afa363ff4ad9751c60f
longkey1 <longkey1@gmail.com>
2025-01-17 13:01:52 +0900

    fix: cloudlared build

--- .dotfiles/cloudflared/build.sh
+++ .dotfiles/cloudflared/build.sh
@@ -2,7 +2,12 @@
 
 BINARY="cloudflared"
 REPOSITORY="cloudflare/cloudflared"
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 if [ ! -x "${LOCAL_BIN}/${BINARY}" ]; then
-  ${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/
+  if [ "${OS}" = "linux" ]; then
+    ${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/ --asset cloudflared-linux-amd64
+  else
+    ${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/
+  fi
 fi
