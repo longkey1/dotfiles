fe3d5f2633fa73865b6d834ec77fbc01963d6a1e
longkey1 <longkey1@gmail.com>
2025-02-13 10:49:20 +0900

    chore: refactor: Improve codegpt build and deployment
    - Added build, clean, install, uninstall, and update scripts for codegpt.
    - Added .codegpt.yaml configuration file and its dist template.
    - Added codegpt configuration to .gitignore.
    - Removed a git commit template.
    - Updated the git config file.

---
+++ .dotfiles/codegpt/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+BINARY="codegpt"
+REPOSITORY="appleboy/CodeGPT"
+
+if [ ! -x "${LOCAL_BIN}/${BINARY}" ]; then
+  ${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/${BINARY} --asset=^.xz
+fi
+
+. ${DOTFILES}/functions
+bw_session=$(get_bitwarden_session)
+
+export GEMINI_API_KEY=$(${LOCAL_BIN}/bw get notes a317aaf3-bb1e-4f52-8f07-b27f010add73 --session "${bw_session}")
+envsubst '${GEMINI_API_KEY}' < ${LOCAL_CONFIG}/codegpt/.codegpt.yaml.dist > ${LOCAL_CONFIG}/codegpt/.codegpt.yaml
---
+++ .dotfiles/codegpt/clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+BINARY="codegpt"
+rm -f ${LOCAL_BIN}/${BINARY}
+rm -f ${LOCAL_CONFIG}/codegpt/.codegpt.yaml
