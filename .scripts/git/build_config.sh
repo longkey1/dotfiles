e4a4f0bde85e1c816218c3f9bb9adea7d723c3e0
longkey1 <longkey1@gmail.com>
2025-06-04 13:08:41 +0900

    feat: Add Gemini API key to git config build
    - Adds the Gemini API key to the git config build process.
    - Retrieves the Bitwarden session and uses it to fetch the key.
    - Updates the `envsubst` command to include the new key.

--- .scripts/git/build_config.sh
+++ .scripts/git/build_config.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env zsh
 
-envsubst '${GPG_KEYID}' < ${LOCAL_CONFIG}/git/config.dist > ${LOCAL_CONFIG}/git/config
+set -a && . ${SCRIPTS}/secrets.env && set +a
+
+. ${SCRIPTS}/functions
+bw_session=$(get_bitwarden_session)
+export GEMINI_API_KEY=$(${LOCAL_BIN}/bw get notes a317aaf3-bb1e-4f52-8f07-b27f010add73 --session "${bw_session}")
+
+envsubst '${GPG_KEYID} ${GEMINI_API_KEY}' < ${LOCAL_CONFIG}/git/config.dist > ${LOCAL_CONFIG}/git/config
