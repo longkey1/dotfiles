ec7f9e07786156a919660769a423059db9c3bfd3
longkey1 <longkey1@gmail.com>
2025-06-13 23:54:15 +0900

    chore(llmc): add build script for llmc
    Added a new build.sh script in the .scripts/llmc directory to support the build process for llmc. This change helps automate and standardize building procedures.

---
+++ .scripts/llmc/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+BINARY="llmc"
+REPOSITORY="longkey1/llmc"
+CURRENT=$(cd $(dirname $0);pwd)
+
+${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/${BINARY} --upgrade-only
+${LOCAL_BIN}/checkexec ${LOCAL_CONFIG}/llmc/config.toml ${LOCAL_CONFIG}/llmc/config.toml.dist -- env LOCAL_CONFIG=${LOCAL_CONFIG} ${CURRENT}/build_config.sh
