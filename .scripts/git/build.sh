41960d29d90acfb5c4ddf7151e478063a3611255
longkey1 <longkey1@gmail.com>
2025-06-28 06:16:15 +0900

    refactor: Streamline config build process in scripts
    - Moved Bitwarden credential fetching logic from `gcal/build.sh` to `gcal/build_config.sh`, centralizing configuration generation concerns.
    - Removed redundant `env LOCAL_CONFIG=${LOCAL_CONFIG}` specification from `checkexec` commands in `build.sh` scripts for `gcal`, `git`, `godl`, `jnal`, and `tmpl`.
    - This change simplifies build scripts by removing unnecessary environment variable passing to sub-scripts.

--- .scripts/git/build.sh
+++ .scripts/git/build.sh
@@ -3,5 +3,4 @@
 CURRENT=$(cd $(dirname $0);pwd)
 
 set -a && . ${SCRIPTS}/secrets.env && set +a
-${LOCAL_BIN}/checkexec ${LOCAL_CONFIG}/git/config ${LOCAL_CONFIG}/git/config.dist ${SCRIPTS}/secrets.env -- env LOCAL_CONFIG=${LOCAL_CONFIG} ${CURRENT}/build_config.sh
-
+${LOCAL_BIN}/checkexec ${LOCAL_CONFIG}/git/config ${LOCAL_CONFIG}/git/config.dist ${SCRIPTS}/secrets.env -- ${CURRENT}/build_config.sh
