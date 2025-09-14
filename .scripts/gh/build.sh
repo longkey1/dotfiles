c5479c9126b082112adae63516109edfb52bd1c1
longkey1 <longkey1@gmail.com>
2025-09-14 23:55:14 +0900

    chore: add /local/lib to .gitignore and fix OS detection in gh build script
    - Add `/local/lib` directory to .gitignore to exclude local library files from version control
    - Fix OS detection in gh build script by declaring OS variable before usage
    - Remove trailing newline from gcal build script

--- .scripts/gh/build.sh
+++ .scripts/gh/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 REPOSITORY="cli/cli"
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 if [ "${OS}" = "linux" ]; then
   ${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/ --upgrade-only --asset tar.gz
