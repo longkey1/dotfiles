90a6bd635072ed1428be5a115ede91a43d6f849b
longkey1 <longkey1@gmail.com>
2025-12-05 16:21:13 +0900

    chore: Use explicit path for envsubst command
    - Prefix `envsubst` commands with `${LOCAL_BIN}/` across several build scripts.
    - Ensures `envsubst` is consistently invoked from a designated local binary directory.
    - Improves script robustness by reducing reliance on the system's `PATH` for the `envsubst` executable.

--- .scripts/jnal/build_config.sh
+++ .scripts/jnal/build_config.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-envsubst '${HOME}' < ${LOCAL_CONFIG}/jnal/config.toml.dist > ${LOCAL_CONFIG}/jnal/config.toml
+${LOCAL_BIN}/envsubst '${HOME}' < ${LOCAL_CONFIG}/jnal/config.toml.dist > ${LOCAL_CONFIG}/jnal/config.toml
