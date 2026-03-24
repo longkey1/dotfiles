b7b07930723e49b02c28bbd8cb3de763c8819b94
longkey1 <longkey1@gmail.com>
2026-03-24 18:10:06 +0900

    fix: remove unnecessary config flag from jnal completion command
    - Remove `--config ${LOCAL_CONFIG}/jnal/config.toml` argument from jnal completion command as it is not required for generating zsh completions

--- scripts/jnal/build.sh
+++ scripts/jnal/build.sh
@@ -7,4 +7,4 @@ CURRENT=$(cd $(dirname $0);pwd)
 ${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/ --upgrade-only
 
 mkdir -p ${LOCAL_CONFIG}/zsh/functions
-${LOCAL_BIN}/jnal --config ${LOCAL_CONFIG}/jnal/config.toml completion zsh > ${LOCAL_CONFIG}/zsh/functions/_jnal
+${LOCAL_BIN}/jnal completion zsh > ${LOCAL_CONFIG}/zsh/functions/_jnal
