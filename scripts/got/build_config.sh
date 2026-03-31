ea5c3cf8c950ed400a1cebbfe1d1b5ab996e6c2c
longkey1 <longkey1@gmail.com>
2026-03-31 14:32:27 +0900

    chore: remove got configuration and scripts
    - Remove got configuration file and temporary directory structure
    - Delete got build, install, update, and clean scripts that are no longer needed

--- scripts/got/build_config.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-APP="got"
-
-${LOCAL_BIN}/envsubst '${HOME}' < ${LOCAL_CONFIG}/${APP}/config.toml.dist > ${LOCAL_CONFIG}/${APP}/config.toml
--- scripts/got/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-APP="got"
-
-. ${SCRIPTS}/functions
-
-symlink local/bin/${APP}
-symlink config/${APP}
