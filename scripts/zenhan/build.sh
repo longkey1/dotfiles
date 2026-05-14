2da3d20cd0200c102f0b52c802ee096d9a0107ae
longkey1 <longkey1@gmail.com>
2026-05-14 13:47:30 +0900

    chore: Update Windows binary path in build script
    - Modified the `build.sh` script to reflect a change in the default installation path for Windows binaries.
    - The `WIN_BIN` variable was updated from "/mnt/c/Users/.../Program Files" to "/mnt/c/Users/.../Program Files Local".

--- scripts/zenhan/build.sh
+++ scripts/zenhan/build.sh
@@ -7,6 +7,6 @@ fi
 DIRECTORY="zenhan"
 REPOSITORY="iuchim/zenhan"
 CURRENT=$(cd $(dirname $0);pwd)
-WIN_BIN="/mnt/c/Users/$(powershell.exe '$env:USERNAME' | tr -d '\r')/Program Files"
+WIN_BIN="/mnt/c/Users/$(powershell.exe '$env:USERNAME' | tr -d '\r')/Program Files Local"
 
 ${LOCAL_BIN}/eget ${REPOSITORY} --file zenhan --to ${WIN_BIN}/ --upgrade-only
