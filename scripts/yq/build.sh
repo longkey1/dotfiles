62b1325eac7165438dde75cce1e0eac60b2d42d7
longkey1 <longkey1@gmail.com>
2026-06-12 11:40:07 +0900

    refactor: simplify yq build script by removing manual OS/arch detection
    - Remove explicit `OS` and `ARCH` variable definitions, delegating auto-detection to `eget`
    - Replace OS/arch-specific asset and file flags with glob patterns (`.tar.gz` and `yq_*`) to let `eget` resolve the correct binary automatically

--- scripts/yq/build.sh
+++ scripts/yq/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 
 REPOSITORY="mikefarah/yq"
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-ARCH=$(uname -m | sed s/x86_64/amd64/)
 
-${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/yq --asset ${OS}_${ARCH}.tar.gz --file yq_${OS}_${ARCH} --upgrade-only
+# OS/arch は eget が自動検出する。--asset で生バイナリを除き tar.gz に絞り、--file で tarball 内のバイナリのみ抽出する。
+${LOCAL_BIN}/eget ${REPOSITORY} --to ${LOCAL_BIN}/yq --asset .tar.gz --file 'yq_*' --upgrade-only
