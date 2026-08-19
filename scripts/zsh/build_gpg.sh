b8fcc7655d03e7b9418ec655e3cecf60489bb371
longkey1 <longkey1@gmail.com>
2026-08-19 15:38:56 +0900

    fix: update bitwarden item id for GPG passphrase
    - Replace outdated bitwarden secret reference with the correct item id when fetching GPG_PASSPHRASE

--- scripts/zsh/build_gpg.sh
+++ scripts/zsh/build_gpg.sh
@@ -4,7 +4,7 @@ set -a && . "${SCRIPTS}"/secrets.env && set +a
 
 . "${SCRIPTS}"/functions
 bw_session=$(get_bitwarden_session)
-GPG_PASSPHRASE=$("${LOCAL_BIN}"/bw get password c5f2ce95-8cd9-41e6-b1ef-afeb00292e4c --session "${bw_session}")
+GPG_PASSPHRASE=$("${LOCAL_BIN}"/bw get password 4bb3c38c-e677-42a7-ad28-f59e80301e15 --session "${bw_session}")
 export GPG_PASSPHRASE
 
 envsubst '${GPG_KEYGRIP} ${GPG_PASSPHRASE}' <"${LOCAL_CONFIG}"/zsh/zshrc.gpg.dist >"${LOCAL_CONFIG}"/zsh/zshrc.gpg
